85dfa63f7cce06e7da0c71882043354a296b6a52
Rishabh Jaiswal <97353246+rishabhjaiswaliit@users.noreply.github.com>
2024-01-29 06:50:42 +0530

    Update mp3Tomp4.sh

--- mp3Tomp4.sh
+++ mp3Tomp4.sh
@@ -6,5 +6,5 @@ done
 
 # INDO
 for file in *.mp3; do         
-  ffmpeg -loop 1 -i black.jpeg -i "$file" -c:a aac -strict experimental -b:a 192k -shortest -vf "scale=256:144,setsar=1:1" "${indo%.mp3}.mp4"
+  ffmpeg -loop 1 -i black.jpeg -i "$file" -c:a aac -strict experimental -b:a 192k -shortest -vf "scale=256:144,setsar=1:1" "indo${file%.mp3}.mp4"
 done
