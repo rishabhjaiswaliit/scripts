d99760c23b0e6778f4bd15cc2aa7bf9fa5926011
Rishabh Jaiswal <97353246+rishabhjaiswaliit@users.noreply.github.com>
2024-03-16 01:36:43 +0530

    Update mp3Tomp4.sh

--- mp3Tomp4.sh
+++ mp3Tomp4.sh
@@ -2,6 +2,10 @@ for file in *.mp3; do
   ffmpeg -loop 1 -i black.jpeg -i "$file" -c:a aac -strict experimental -b:a 192k -shortest -vf "scale=256:144,setsar=1:1" "${file%.mp3}.mp4"
 done
 
+# output.wav
+for file in output.wav; do         
+  ffmpeg -loop 1 -i black.jpeg -i "$file" -c:a aac -strict experimental -b:a 192k -shortest -vf "scale=256:144,setsar=1:1" "audio-${file%.mp3}.mp4"
+done
 
 
 # JAP
